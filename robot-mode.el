efb3868b62cfe7de6c3c9e9c6096f0f33c59ee6d
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-09-20 19:06:26 +0700

    extract header keywords from font-lock-keywords

--- robot-mode.el
+++ robot-mode.el
@@ -1,7 +1,11 @@
+(defvar robot-mode-header-keywords
+  '("^\\*\\{3\\}\s+\\(Settings\\|Test Cases\\|Keywords\\)\s+\\*\\{3\\}" . font-lock-keyword-face)
+  "Header keywords")
+
 (defvar robot-mode-font-lock-keywords
   (list
-    '("^\\*\\{3\\}\s+\\(Settings\\|Test Cases\\|Keywords\\)\s+\\*\\{3\\}" . font-lock-keyword-face))
-  "Header keywords")
+    robot-mode-header-keywords)
+  "All available keywords")
 
 (define-derived-mode robot-mode fundamental-mode "Robot Framework"
   "A major mode for Robot Framework."
